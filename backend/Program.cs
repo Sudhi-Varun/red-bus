7575bda7f87410380cbb9359ebf373d5c9a3d6d7
Sudhi-Varun <sudhivarun1988@gmail.com>
2023-12-18 12:46:40 +0530

    red-bus

---
+++ backend/Program.cs
@@ -0,0 +1,48 @@
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http:localhost","127.0.0.1");
+                      });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapGet("/test/{val}", (string val) =>
+{
+    var sample = $"Hi {val}";
+    return sample;
+})
+.WithName("test")
+.WithOpenApi();
+
+app.Run();
+
+
